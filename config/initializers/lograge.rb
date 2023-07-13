e37c8e405f9e9b311449852ea8c6543b234b7211
Steve Hill <steve@stevehill.xyz>
2023-07-13 16:42:48 +0100

    Install and configure Lograge

---
+++ config/initializers/lograge.rb
@@ -0,0 +1,18 @@
+Rails.application.configure do
+  config.lograge.enabled = true
+  config.lograge.formatter = Lograge::Formatters::Json.new
+
+  config.lograge.custom_payload do |controller|
+    metadata = {
+      timestamp: Time.now.iso8601,
+      remote_ip: controller.request.remote_ip,
+      request_id: controller.request.request_id
+    }
+
+    if controller.try(:current_user, :present?)
+      metadata[:user_id] = controller.current_user.id
+    end
+
+    metadata
+  end
+end
